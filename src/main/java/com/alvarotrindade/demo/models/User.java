60748179e558967dd3dc0639cd653c15745b3789
Alvaro <alvarotboliveira@gmail.com>
2024-10-25 17:09:47 -0300

    extencion recomendations

---
+++ src/main/java/com/alvarotrindade/demo/models/User.java
@@ -0,0 +1,76 @@
+package com.alvarotrindade.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.scheduling.config.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table (name =User.TABLE_NAME)
+
+public class User {
+     public interface CreateUser {}
+    public interface UpdateUser {}
+
+  public static final String TABLE_NAME = "user";
+
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  private Long id;
+
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty (groups = CreateUser.class)
+    @Size(groups = CreateUser.class,min = 2, max = 100)
+
+    private String Username;
+
+   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+   @Column(name = "password",   length = 60, nullable = false)
+   @NotNull ( groups = {CreateUser.class, UpdateUser.class})
+   @NotEmpty (groups = {CreateUser.class, UpdateUser.class})
+   @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+   private String password;
+
+
+   // private List<Task> tasks = new ArrayList<Task>();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (!(o instanceof User))
+            return false;
+        User other =(User) o;
+        if (this.id == null)
+            if (other.id != null)
+                return false;
+        else if (!this.id.equals(other.id))
+            return false;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.Username, other.Username) && Objects.equals(this.password, other.password);
+
+
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+           int result = 1;
+           result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+           return result;
+
+
+    }
+}
